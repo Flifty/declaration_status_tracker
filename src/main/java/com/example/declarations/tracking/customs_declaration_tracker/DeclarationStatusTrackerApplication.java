1abd952eb3c23c7fa0a0c1614acd313b37e667db
Flifty <danya.leshok@gmail.com>
2025-06-07 00:22:22 +0300

    Initial commit

---
+++ src/main/java/com/example/declarations/tracking/customs_declaration_tracker/DeclarationStatusTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.declarations.tracking.customs_declaration_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeclarationStatusTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeclarationStatusTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/declarations/tracking/customs_declaration_tracker/DeclarationStatusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.declarations.tracking.customs_declaration_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeclarationStatusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
